26007344e55b76136ded013d51d4b401ecbf4dca
Emanuel Maia <emanuel@hnwc.com.br>
2022-04-05 08:02:42 -0300

    feat: useImperativeHandle

---
+++ src/AppHandle.tsx
@@ -0,0 +1,13 @@
+import { useRef } from "react";
+import Form, { FormRef } from "./Form";
+
+const App: React.FC = () => {
+  const formRef = useRef<FormRef>(null);
+
+  function handleSubmit() {
+    formRef.current?.submit();
+  }
+  return <Form ref={formRef} />;
+};
+
+export default App;
